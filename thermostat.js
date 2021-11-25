ba1718890e5b41b12ad89fd328ff4798d71ab306
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2021-11-25 15:02:58 +0000

    minimum temp and max set

--- thermostat.js
+++ thermostat.js
@@ -1,15 +1,41 @@
 class Thermostat{
     constructor() {
         this.temperature = 20;
+        this.powerSaving = true;
+        this.maxTemp = 25;
+        this.minTemp = 10;
     }
     getTemperature() {
         return this.temperature;
     }
     up() {
-        return this.temperature += 1;
+        if (this.temperature >= this.maxTemp) {
+            return this.temperature;
+        }
+        else {
+          return this.temperature += 1;
+        }
+        
     }
     down() {
+        if (this.temperature <= this.minTemp) {
+            return this.temperature;
+        }
+        else {
         return this.temperature -= 1;
+        }
+    }
+    
+    setPowerSavingMode(boolean) {
+        this.powerSaving = boolean;
+        this.setMaxTemp();
+    }
+    setMaxTemp() {
+        if(this.powerSaving) {
+          this.maxTemp = 25;
+        } else {
+          this.maxTemp = 32;
+        }
     }
 
 }
