4636ea227d7d106c4b897f7c75af40845fd3f0e2
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2021-11-28 11:08:08 +0000

    cli functions

--- cli.js
+++ cli.js
@@ -3,3 +3,26 @@ const Thermostat = require("./thermostat.js");
 
 const thermostat = new Thermostat();
 console.log(`Temperature is ${thermostat.getTemperature()}`)
+
+while (true) {
+    let command = readlineSync.question("Enter your command > ");
+      if(command === "up"){
+        thermostat.up();
+        console.log(`Temperature is ${thermostat.getTemperature()}`);
+      }
+      else if(command === "down") { 
+        thermostat.down();   
+        console.log(`Temperature is ${thermostat.getTemperature()}`);     
+      } 
+      else if (command === 'psm on'){
+        thermostat.setPowerSavingMode(true);
+        console.log(`Temperature is ${thermostat.getTemperature()}`);
+      } 
+      else if(command === 'psm off') {
+        thermostat.setPowerSavingMode(false);
+        console.log(`Temperature is ${thermostat.getTemperature()}`);
+      }
+      else if (command === 'quit' & command === 'exit' ) {
+          break;
+      }
+}; 
